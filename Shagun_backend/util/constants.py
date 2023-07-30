dac3b71f8c21d68f17c411a47256d41835146721
Roop Raj <rooprajt@gmail.com>
2023-07-30 13:31:54 +0530

    initial commit for week 4

--- Shagun_backend/util/constants.py
+++ Shagun_backend/util/constants.py
@@ -19,16 +19,16 @@ quotes = [
 ]
 
 wishes = [
-    "Let Shagun be the radiant bridge that connects souls, transcending boundaries and uniting lives in a tapestry of blessings.",
-    "With Shagun, celebrate the joy of giving and the beauty of spreading love on every occasion.",
-    "Embrace the spirit of togetherness and create unforgettable moments with Shagun.",
-    "At Shagun, we believe in making every occasion a cherished memory to be treasured forever.",
-    "Discover the perfect harmony of tradition and modernity with Shagun, as we celebrate the essence of every occasion.",
-    "Let Shagun be your companion in sharing happiness and creating lasting memories with your loved ones.",
-    "Experience the magic of heartfelt gifting with Shagun, where every occasion becomes an extraordinary celebration.",
-    "With Shagun, let your gestures of love and thoughtfulness illuminate every special moment in life.",
-    "At Shagun, we strive to make your celebrations extraordinary, as we cherish the beauty of every occasion.",
-    "Unlock the joy of giving and make every occasion a remarkable journey with Shagun."
+    "Dear ABC, as you begin this beautiful journey together, may your love shine brighter with each passing day, and may your hearts forever be entwined in happiness and joy.",
+    "Dear ABC, on this special day, we wish you a lifetime of love and laughter, and may your love story inspire others to find their own happily ever after.",
+    "Dear ABC, as you take each step hand in hand, may you always find comfort and strength in each other, and may your love grow deeper and more profound with every moment you share.",
+    "Dear ABC, on your wedding day, we wish you a future filled with endless love, endless adventures, and endless memories to cherish forever.",
+    "Dear ABC, may the love you share today blossom into an extraordinary bond that brings you both immense happiness and fulfillment throughout your lives.",
+    "Dear ABC, as you embark on this new chapter, may your love be a guiding light, leading you through every joy and every challenge, and may your commitment to each other only grow stronger with time.",
+    "Dear ABC, may your wedding day be just the beginning of a beautiful love story, filled with laughter, understanding, and the magic of togetherness.",
+    "Dear ABC, as you say your vows, know that your love has touched the hearts of everyone around you, and we wish you a lifetime of blessings and unconditional love.",
+    "Dear ABC, as you unite your lives, may you find in each other a best friend, a confidante, and a soulmate, and may your love be a source of comfort and inspiration.",
+    "Dear ABC, may your marriage be a journey of growth and discovery, where you continue to fall in love with each other every day, creating a bond that lasts a lifetime."
 ]
 
 

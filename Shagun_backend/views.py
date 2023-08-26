ed03553ffc00b0d353881a97768f77b12255791f
Roop Raj <rooprajt@gmail.com>
2023-08-26 15:51:24 +0530

    initial commit for week 5

--- Shagun_backend/views.py
+++ Shagun_backend/views.py
@@ -1527,24 +1527,21 @@ def request_callback(request):
 
 @api_view(['POST'])
 def add_transaction_history(request):
-    transaction_obj = transactions_history_model.transactions_history_model_from_dict(request.data)
-    response, status_code = transactions_controller.add_transaction_history(transaction_obj)
-    return JsonResponse(response, status=status_code)
-    # token = request.headers.get('Authorization').split(' ')[1]
-    # try:
-    #     decoded_token = jwt.decode(token, 'secret_key', algorithms=['HS256'])
-    #     username = decoded_token['username']
-    #     if username == request.data.get('uid'):
-    #         transaction_obj = transactions_history_model.transactions_history_model_from_dict(request.data)
-    #         response, status_code = transactions_controller.add_transaction_history(transaction_obj)
-    #         return JsonResponse(response, status=status_code)
-    #     else:
-    #         return JsonResponse({'message': 'Invalid token for user'}, status=401)
-    #
-    # except jwt.ExpiredSignatureError:
-    #     return JsonResponse({'message': 'Token has expired'}, status=401)
-    # except jwt.InvalidTokenError:
-    #     return JsonResponse({'message': 'Invalid token'}, status=401)
+    token = request.headers.get('Authorization').split(' ')[1]
+    try:
+        decoded_token = jwt.decode(token, 'secret_key', algorithms=['HS256'])
+        username = decoded_token['username']
+        if username == request.data.get('uid'):
+            transaction_obj = transactions_history_model.transactions_history_model_from_dict(request.data)
+            response, status_code = transactions_controller.add_transaction_history(transaction_obj)
+            return JsonResponse(response, status=status_code)
+        else:
+            return JsonResponse({'message': 'Invalid token for user'}, status=401)
+
+    except jwt.ExpiredSignatureError:
+        return JsonResponse({'message': 'Token has expired'}, status=401)
+    except jwt.InvalidTokenError:
+        return JsonResponse({'message': 'Invalid token'}, status=401)
 
 
 @api_view(['POST'])
@@ -1567,25 +1564,22 @@ def user_home_page(request):
 
 @api_view(['POST'])
 def gift_sent_list(request):
-    gift_data_obj = gifts_transaction_model.gifts_transaction_model_from_dict(request.data)
-    response, status_code = transactions_controller.get_sent_gift(gift_data_obj)
-    return JsonResponse(response, status=status_code)
-    # token = request.headers.get('Authorization').split(' ')[1]
-    # try:
-    #     decoded_token = jwt.decode(token, 'secret_key', algorithms=['HS256'])
-    #     username = decoded_token['username']
-    #     if username == request.data.get('uid'):
-    #         gift_data_obj = gifts_transaction_model.gifts_transaction_model_from_dict(request.data)
-    #         response, status_code = transactions_controller.get_sent_gift(gift_data_obj)
-    #         return JsonResponse(response, status=status_code)
-    #
-    #     else:
-    #         return JsonResponse({'message': 'Invalid token for user'}, status=401)
-    #
-    # except jwt.ExpiredSignatureError:
-    #     return JsonResponse({'message': 'Token has expired'}, status=401)
-    # except jwt.InvalidTokenError:
-    #     return JsonResponse({'message': 'Invalid token'}, status=401)
+    token = request.headers.get('Authorization').split(' ')[1]
+    try:
+        decoded_token = jwt.decode(token, 'secret_key', algorithms=['HS256'])
+        username = decoded_token['username']
+        if username == request.data.get('uid'):
+            gift_data_obj = gifts_transaction_model.gifts_transaction_model_from_dict(request.data)
+            response, status_code = transactions_controller.get_sent_gift(gift_data_obj)
+            return JsonResponse(response, status=status_code)
+
+        else:
+            return JsonResponse({'message': 'Invalid token for user'}, status=401)
+
+    except jwt.ExpiredSignatureError:
+        return JsonResponse({'message': 'Token has expired'}, status=401)
+    except jwt.InvalidTokenError:
+        return JsonResponse({'message': 'Invalid token'}, status=401)
 
 
 @api_view(['POST'])

c258dc3ce5bd2a98a69a737e25f9f55c5db93eed
Roop Raj <rooprajt@gmail.com>
2023-09-16 12:14:03 +0530

    initial commit for week 5

--- Shagun_backend/models/gifts_transaction_model.py
+++ Shagun_backend/models/gifts_transaction_model.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, TypeVar, Type, cast
+from typing import Optional, Any, TypeVar, Type, cast
 
 
 T = TypeVar("T")
@@ -10,6 +10,20 @@ def from_str(x: Any) -> str:
     return x
 
 
+def from_none(x: Any) -> Any:
+    assert x is None
+    return x
+
+
+def from_union(fs, x):
+    for f in fs:
+        try:
+            return f(x)
+        except:
+            pass
+    assert False
+
+
 def to_class(c: Type[T], x: Any) -> dict:
     assert isinstance(x, c)
     return cast(Any, x).to_dict()
@@ -17,23 +31,30 @@ def to_class(c: Type[T], x: Any) -> dict:
 
 @dataclass
 class GiftsTransactionModel:
-    uid: str
-    type: str
-    month: str
+    uid: Optional[str] = None
+    type: Optional[str] = None
+    month: Optional[str] = None
+    search: Optional[str] = None
 
     @staticmethod
     def from_dict(obj: Any) -> 'GiftsTransactionModel':
         assert isinstance(obj, dict)
-        uid = from_str(obj.get("uid"))
-        type = from_str(obj.get("type"))
-        month = from_str(obj.get("month"))
-        return GiftsTransactionModel(uid, type, month)
+        uid = from_union([from_str, from_none], obj.get("uid"))
+        type = from_union([from_str, from_none], obj.get("type"))
+        month = from_union([from_str, from_none], obj.get("month"))
+        search = from_union([from_str, from_none], obj.get("search"))
+        return GiftsTransactionModel(uid, type, month, search)
 
     def to_dict(self) -> dict:
         result: dict = {}
-        result["uid"] = from_str(self.uid)
-        result["type"] = from_str(self.type)
-        result["month"] = from_str(self.month)
+        if self.uid is not None:
+            result["uid"] = from_union([from_str, from_none], self.uid)
+        if self.type is not None:
+            result["type"] = from_union([from_str, from_none], self.type)
+        if self.month is not None:
+            result["month"] = from_union([from_str, from_none], self.month)
+        if self.search is not None:
+            result["search"] = from_union([from_str, from_none], self.search)
         return result
 
 

461519c6c659e243632c0280afc284d34b96d852
santhosh kumar <santhosh.kumar@conigitalgroup.com>
2023-09-24 23:22:50 +0530

    notification redundant space removed

--- Shagun_backend/controllers/transactions_controller.py
+++ Shagun_backend/controllers/transactions_controller.py
@@ -309,8 +309,7 @@ def settle_payment(transactions_list):
                     invite_notification_query = f"""INSERT INTO notification (uid, type, title, message) 
                                                     VALUES ('{receiver}', 'shagun',
                                                     'Shagun amount {total_amount} credited by {sender_name}',
-                                                    'Shagun amount of {total_amount} INR has been successfully 
-                                                    transferred to {bank_name} Bank for the account ending with 
+                                                    'Shagun amount of {total_amount} INR has been successfully transferred to {bank_name} Bank for the account ending with 
                                                     {'*' * (len(str(account_number)) - 4) + str(account_number)[-4:]}')"""
                     cursor.execute(invite_notification_query)
                     title = f"""Shagun amount {total_amount} credited from {sender_name}"""

e3ec0fd2a79ded7e0e6defe9be5124b6f9c2be18
santhosh kumar <santhosh.kumar@conigitalgroup.com>
2023-09-24 21:28:14 +0530

    migrate file rectifued

--- migrate_db.py
+++ migrate_db.py
@@ -13,54 +13,54 @@ print(password_hash)
 table_queries = [
     """
     CREATE TABLE IF NOT EXISTS bank_details (
-        id INT(11) NOT NULL AUTO_INCREMENT,
-        uid VARCHAR(255) NOT NULL,
-        bank_name VARCHAR(255) NOT NULL,
-        bank_logo VARCHAR(255) NOT NULL,
-        ifsc_code VARCHAR(255) NOT NULL,
-        account_holder_name VARCHAR(255) NOT NULL,
-        account_number VARCHAR(255) NOT NULL,
-        status TINYINT(1) NOT NULL,
-        created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-        added_by VARCHAR(255) NOT NULL,
-        modified_on DATETIME NOT NULL,
-        modified_by VARCHAR(255) NOT NULL,
+        id INT(11)  AUTO_INCREMENT,
+        uid VARCHAR(255) ,
+        bank_name VARCHAR(255) ,
+        bank_logo VARCHAR(255) ,
+        ifsc_code VARCHAR(255) ,
+        account_holder_name VARCHAR(255) ,
+        account_number VARCHAR(255) ,
+        status TINYINT(1) ,
+        created_on TIMESTAMP  DEFAULT CURRENT_TIMESTAMP(),
+        added_by VARCHAR(255) ,
+        modified_on DATETIME ,
+        modified_by VARCHAR(255) ,
         PRIMARY KEY (id)
     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
     """,
     """
     CREATE TABLE `bank_list` (
-         `id` int(11) NOT NULL AUTO_INCREMENT,
-         `bank_name` varchar(255) NOT NULL,
-         `bank_logo` varchar(255) NOT NULL,
-         `status` tinyint(1) NOT NULL,
-         `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
-         `created_by` varchar(255) NOT NULL,
+         `id` int(11)  AUTO_INCREMENT,
+         `bank_name` varchar(255) ,
+         `bank_logo` varchar(255) ,
+         `status` tinyint(1) ,
+         `created_on` timestamp  DEFAULT current_timestamp(),
+         `created_by` varchar(255) ,
          PRIMARY KEY (`id`)
     ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE IF NOT EXISTS delivery_vendors (
-        id INT(11) NOT NULL AUTO_INCREMENT,
-        delivery_vendor_name VARCHAR(255) NOT NULL,
-        city INT(255) NOT NULL,
-        address VARCHAR(255) NOT NULL,
-        lat_lng VARCHAR(255) NOT NULL,
-        status TINYINT(1) NOT NULL DEFAULT 1,
-        gst_no VARCHAR(255) NOT NULL,
-        delivery_vendor_owner VARCHAR(255) NOT NULL,
-        contact_number VARCHAR(255) NOT NULL,
-        created_by VARCHAR(255) NOT NULL,
-        created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+        id INT(11)  AUTO_INCREMENT,
+        delivery_vendor_name VARCHAR(255) ,
+        city INT(255) ,
+        address VARCHAR(255) ,
+        lat_lng VARCHAR(255) ,
+        status TINYINT(1)  DEFAULT 1,
+        gst_no VARCHAR(255) ,
+        delivery_vendor_owner VARCHAR(255) ,
+        contact_number VARCHAR(255) ,
+        created_by VARCHAR(255) ,
+        created_on TIMESTAMP  DEFAULT CURRENT_TIMESTAMP(),
         PRIMARY KEY (id)
     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
     """,
 
     """ 
     CREATE TABLE `django_session` (
- `session_key` varchar(40) NOT NULL,
- `session_data` longtext NOT NULL,
- `expire_date` datetime(6) NOT NULL,
+ `session_key` varchar(40) ,
+ `session_data` longtext ,
+ `expire_date` datetime(6) ,
  PRIMARY KEY (`session_key`),
  KEY `django_session_expire_date_a5c62663` (`expire_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
@@ -68,105 +68,105 @@ table_queries = [
 
     """ 
     CREATE TABLE `event` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `created_by_uid` varchar(255) NOT NULL,
- `event_type_id` int(11) NOT NULL,
- `city_id` int(11) NOT NULL,
- `address_line1` varchar(255) NOT NULL,
- `address_line2` varchar(255) NOT NULL,
- `event_lat_lng` varchar(255) NOT NULL,
- `created_on` datetime NOT NULL,
- `sub_events` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`sub_events`)),
- `event_date` datetime NOT NULL,
- `event_note` varchar(255) NOT NULL,
- `event_admin` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`event_admin`)),
- `is_approved` tinyint(1) NOT NULL DEFAULT 0,
- `approved_by` int(11) NOT NULL,
- `printer_id` int(11) NOT NULL,
- `approved_date_time` datetime NOT NULL,
- `status` tinyint(1) NOT NULL DEFAULT 0,
- `delivery_fee` double NOT NULL,
- `delivery_address` varchar(255) NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `created_by_uid` varchar(255) ,
+ `event_type_id` int(11) ,
+ `city_id` int(11) ,
+ `address_line1` varchar(255) ,
+ `address_line2` varchar(255) ,
+ `event_lat_lng` varchar(255) ,
+ `created_on` datetime ,
+ `sub_events` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  CHECK (json_valid(`sub_events`)),
+ `event_date` datetime ,
+ `event_note` varchar(255) ,
+ `event_admin` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  CHECK (json_valid(`event_admin`)),
+ `is_approved` tinyint(1)  DEFAULT 0,
+ `approved_by` int(11) ,
+ `printer_id` int(11) ,
+ `approved_date_time` datetime ,
+ `status` tinyint(1)  DEFAULT 0,
+ `delivery_fee` double ,
+ `delivery_address` varchar(255) ,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=221 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE `events_type` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `event_type_name` varchar(255) NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `event_type_name` varchar(255) ,
  `status` tinyint(1) DEFAULT 1,
- `created_by` varchar(255) NOT NULL,
+ `created_by` varchar(255) ,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE `event_admin_invite` (
- `id` int(11) NOT NULL,
- `invited_by` varchar(255) NOT NULL,
- `invited_to` varchar(255) NOT NULL,
- `event_id` int(11) NOT NULL,
- `status` tinyint(1) NOT NULL DEFAULT 0,
- `created_at` int(11) NOT NULL
+ `id` int(11) ,
+ `invited_by` varchar(255) ,
+ `invited_to` varchar(255) ,
+ `event_id` int(11) ,
+ `status` tinyint(1)  DEFAULT 0,
+ `created_at` int(11) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE `event_guest_invite` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `invited_by` varchar(255) NOT NULL,
- `invited_to` varchar(255) NOT NULL,
- `event_id` int(11) NOT NULL,
- `status` tinyint(1) NOT NULL DEFAULT 0,
- `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
- `invite_message` varchar(500) NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `invited_by` varchar(255) ,
+ `invited_to` varchar(255) ,
+ `event_id` int(11) ,
+ `status` tinyint(1)  DEFAULT 0,
+ `created_at` timestamp  DEFAULT current_timestamp(),
+ `invite_message` varchar(500) ,
  PRIMARY KEY (`id`),
  UNIQUE KEY `unique_invite` (`invited_by`,`invited_to`,`event_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE `greeting_cards` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `card_name` varchar(255) NOT NULL,
- `card_image_url` varchar(255) NOT NULL,
- `card_price` double NOT NULL,
- `printer_id` int(11) NOT NULL,
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
- `status` tinyint(1) NOT NULL DEFAULT 1,
+ `id` int(11)  AUTO_INCREMENT,
+ `card_name` varchar(255) ,
+ `card_image_url` varchar(255) ,
+ `card_price` double ,
+ `printer_id` int(11) ,
+ `created_on` timestamp  DEFAULT current_timestamp(),
+ `status` tinyint(1)  DEFAULT 1,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE `locations` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `city_name` varchar(255) NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `city_name` varchar(255) ,
  `status` tinyint(1) DEFAULT 1,
- `created_by` varchar(255) NOT NULL,
+ `created_by` varchar(255) ,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE `notification` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `uid` varchar(255) NOT NULL,
- `type` varchar(255) NOT NULL,
- `title` text NOT NULL,
- `message` text NOT NULL,
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+ `id` int(11)  AUTO_INCREMENT,
+ `uid` varchar(255) ,
+ `type` varchar(255) ,
+ `title` text ,
+ `message` text ,
+ `created_on` timestamp  DEFAULT current_timestamp(),
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE `printer` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `store_name` varchar(255) NOT NULL,
- `city` int(11) NOT NULL,
- `address` varchar(255) NOT NULL,
- `email` varchar(255) NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `store_name` varchar(255) ,
+ `city` int(11) ,
+ `address` varchar(255) ,
+ `email` varchar(255) ,
  `status` tinyint(1) DEFAULT 1,
- `gst_no` varchar(255) NOT NULL,
- `store_owner` varchar(255) NOT NULL,
- `contact_number` varchar(255) NOT NULL,
- `printer_user_name` varchar(255) NOT NULL,
- `printer_password` varchar(255) NOT NULL,
+ `gst_no` varchar(255) ,
+ `store_owner` varchar(255) ,
+ `contact_number` varchar(255) ,
+ `printer_user_name` varchar(255) ,
+ `printer_password` varchar(255) ,
  PRIMARY KEY (`id`),
  UNIQUE KEY `uk_printer_user_name` (`printer_user_name`),
  KEY `idx_printer_user_name` (`printer_user_name`)
@@ -174,69 +174,69 @@ table_queries = [
     """,
     """
     CREATE TABLE `print_jobs` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `transaction_id` varchar(255) NOT NULL,
- `printer_id` varchar(255) NOT NULL,
- `card_id` varchar(255) NOT NULL,
- `status` tinyint(1) NOT NULL,
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
- `last_modified` datetime NOT NULL,
- `billing_amount` double NOT NULL,
- `event_id` int(11) NOT NULL,
- `wish` longtext NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `transaction_id` varchar(255) ,
+ `printer_id` varchar(255) ,
+ `card_id` varchar(255) ,
+ `status` tinyint(1) ,
+ `created_on` timestamp  DEFAULT current_timestamp(),
+ `last_modified` datetime ,
+ `billing_amount` double ,
+ `event_id` int(11) ,
+ `wish` longtext ,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
 
     """
     CREATE TABLE `settlements` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `transaction_id` int(11) NOT NULL,
- `event_id` int(11) NOT NULL,
- `receiver_bank_id` int(11) NOT NULL,
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+ `id` int(11)  AUTO_INCREMENT,
+ `transaction_id` int(11) ,
+ `event_id` int(11) ,
+ `receiver_bank_id` int(11) ,
+ `created_on` timestamp  DEFAULT current_timestamp(),
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
 
     """
     CREATE TABLE `transaction_history` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `sender_uid` varchar(255) NOT NULL,
- `receiver_uid` varchar(255) NOT NULL,
- `transaction_amount` double NOT NULL,
- `shagun_amount` double NOT NULL,
- `greeting_card_id` int(11) NOT NULL,
- `transaction_fee` double NOT NULL,
- `delivery_fee` double NOT NULL,
- `transaction_id` varchar(255) NOT NULL,
- `payment_status` varchar(255) NOT NULL,
- `event_id` int(11) NOT NULL,
- `status` tinyint(1) NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `sender_uid` varchar(255) ,
+ `receiver_uid` varchar(255) ,
+ `transaction_amount` double ,
+ `shagun_amount` double ,
+ `greeting_card_id` int(11) ,
+ `transaction_fee` double ,
+ `delivery_fee` double ,
+ `transaction_id` varchar(255) ,
+ `payment_status` varchar(255) ,
+ `event_id` int(11) ,
+ `status` tinyint(1) ,
  `is_settled` tinyint(1) DEFAULT 0,
- `reciever_bank_id` varchar(255) NOT NULL,
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
- `gifter_name` varchar(255) NOT NULL,
- `greeting_card_price` double NOT NULL,
+ `reciever_bank_id` varchar(255) ,
+ `created_on` timestamp  DEFAULT current_timestamp(),
+ `gifter_name` varchar(255) ,
+ `greeting_card_price` double ,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
 
     """
     CREATE TABLE `users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `uid` varchar(255) NOT NULL,
- `name` varchar(255) NOT NULL,
- `email` varchar(255) NOT NULL,
- `phone` varchar(255) NOT NULL,
- `auth_type` varchar(255) NOT NULL,
- `kyc` tinyint(1) NOT NULL DEFAULT 0,
- `profile_pic` varchar(255) NOT NULL DEFAULT 'http://cdn.onlinewebfonts.com/svg/img_504768.png',
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+ `id` int(11)  AUTO_INCREMENT,
+ `uid` varchar(255) ,
+ `name` varchar(255) ,
+ `email` varchar(255) ,
+ `phone` varchar(255) ,
+ `auth_type` varchar(255) ,
+ `kyc` tinyint(1)  DEFAULT 0,
+ `profile_pic` varchar(255)  DEFAULT 'http://cdn.onlinewebfonts.com/svg/img_504768.png',
+ `created_on` timestamp  DEFAULT current_timestamp(),
  `status` tinyint(1) DEFAULT 1,
- `role` tinyint(1) NOT NULL,
- `fcm_token` varchar(255) NOT NULL,
- `city` varchar(255) NOT NULL,
+ `role` tinyint(1) ,
+ `fcm_token` varchar(255) ,
+ `city` varchar(255) ,
  `password` varchar(255) DEFAULT NULL,
  PRIMARY KEY (`id`),
  UNIQUE KEY `unique_uid` (`uid`),
@@ -247,45 +247,45 @@ table_queries = [
 
     """
     CREATE TABLE `user_callback_request` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `uid` varchar(255) NOT NULL,
- `type` varchar(255) NOT NULL,
- `status` tinyint(1) NOT NULL DEFAULT 0,
- `completed_by` varchar(255) NOT NULL,
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+ `id` int(11)  AUTO_INCREMENT,
+ `uid` varchar(255) ,
+ `type` varchar(255) ,
+ `status` tinyint(1)  DEFAULT 0,
+ `completed_by` varchar(255) ,
+ `created_on` timestamp  DEFAULT current_timestamp(),
  `event_date` date DEFAULT NULL,
  `event_type` varchar(255) DEFAULT NULL,
- `city` int(11) NOT NULL,
+ `city` int(11) ,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
 
     """
     CREATE TABLE `user_kyc` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `uid` varchar(255) NOT NULL,
- `full_name` varchar(255) NOT NULL,
- `dob` varchar(255) NOT NULL,
- `gender` varchar(255) NOT NULL,
- `address_line1` varchar(255) NOT NULL,
- `address_line2` varchar(255) NOT NULL,
- `city` varchar(255) NOT NULL,
- `state` varchar(255) NOT NULL,
- `postcode` varchar(255) NOT NULL,
- `country` varchar(255) NOT NULL,
- `identification_proof1` varchar(255) NOT NULL,
- `identification_proof2` varchar(255) NOT NULL,
- `identification_number1` varchar(255) NOT NULL,
- `identification_number2` varchar(255) NOT NULL,
- `identification_doc1` varchar(255) NOT NULL,
- `identification_doc2` varchar(255) NOT NULL,
- `verification_status` tinyint(1) NOT NULL,
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
- `created_by` varchar(255) NOT NULL,
- `updated_by` varchar(255) NOT NULL,
- `updated_on` datetime NOT NULL,
- `approved_by` varchar(255) NOT NULL,
- `approved_on` datetime NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `uid` varchar(255) ,
+ `full_name` varchar(255) ,
+ `dob` varchar(255) ,
+ `gender` varchar(255) ,
+ `address_line1` varchar(255) ,
+ `address_line2` varchar(255) ,
+ `city` varchar(255) ,
+ `state` varchar(255) ,
+ `postcode` varchar(255) ,
+ `country` varchar(255) ,
+ `identification_proof1` varchar(255) ,
+ `identification_proof2` varchar(255) ,
+ `identification_number1` varchar(255) ,
+ `identification_number2` varchar(255) ,
+ `identification_doc1` varchar(255) ,
+ `identification_doc2` varchar(255) ,
+ `verification_status` tinyint(1) ,
+ `created_on` timestamp  DEFAULT current_timestamp(),
+ `created_by` varchar(255) ,
+ `updated_by` varchar(255) ,
+ `updated_on` datetime ,
+ `approved_by` varchar(255) ,
+ `approved_on` datetime ,
  PRIMARY KEY (`id`),
  UNIQUE KEY `idx_uid` (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
@@ -293,22 +293,22 @@ table_queries = [
 
     """
     CREATE TABLE `version_details` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `app_name` varchar(255) NOT NULL,
- `min_version` varchar(255) NOT NULL,
- `latest_version` varchar(255) NOT NULL,
- `platform` varchar(255) NOT NULL,
- `created` timestamp NOT NULL DEFAULT current_timestamp(),
- `updated` datetime NOT NULL,
+ `id` int(11)  AUTO_INCREMENT,
+ `app_name` varchar(255) ,
+ `min_version` varchar(255) ,
+ `latest_version` varchar(255) ,
+ `platform` varchar(255) ,
+ `created` timestamp  DEFAULT current_timestamp(),
+ `updated` datetime ,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
     """
     CREATE TABLE `order_status` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `transaction_id` int(11) NOT NULL,
- `status` int(11) NOT NULL,
- `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+ `id` int(11)  AUTO_INCREMENT,
+ `transaction_id` int(11) ,
+ `status` int(11) ,
+ `created_on` timestamp  DEFAULT current_timestamp(),
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
     """,
@@ -328,7 +328,6 @@ try:
         user=user,
         password=password,
         database=database,
-        cursorclass=pymysql.cursors.DictCursor
     )
 
     # Create a cursor object
@@ -343,7 +342,3 @@ try:
 
 except pymysql.Error as e:
     print(f"Error: {e}")
-
-finally:
-    if connection:
-        connection.close()

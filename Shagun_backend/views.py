73e7811b91d00efdac47fd40b08e76c93174ce6d
Roop Raj <rooprajt@gmail.com>
2023-10-15 14:25:01 +0530

    final push with credentials fix and bug fix

--- Shagun_backend/views.py
+++ Shagun_backend/views.py
@@ -64,44 +64,6 @@ def custom_404(request, slug=None):
 
 def admin_dashboard(request):
     if request.session.get('is_logged_in') is not None and request.session.get('is_logged_in') is True:
-        json_data = {
-            "created_by_uid": "admin@shagun.com",
-            "event_type_id": "13",
-            "city_id": "12",
-            "printer_id": "21",
-            "address_line1": "4rd Cross",
-            "address_line2": "#A148",
-            "event_lat_lng": "Latitude: 15.3647083, Longitude: 75.1239547",
-            "sub_events": [
-                {
-                    "sub_event_name": "test",
-                    "start_time": "2023-10-01 23:19:00",
-                    "end_time": "2023-10-01 23:20:00"
-                }
-            ],
-            "event_date": "2023-10-01 23:19:00",
-            "event_note": "test event",
-            "event_admin": [
-                {
-                    "name": "David Willey",
-                    "role": "test1",
-                    "uid": "wjkkjhgfdserty",
-                    "profile": "images/profile_pic/circular_logo.png",
-                    "QR_code": "qr code"
-                },
-                {
-                    "name": "David Willey",
-                    "role": "test1",
-                    "uid": "wjkkjhgfdserty",
-                    "profile": "images/profile_pic/circular_logo.png",
-                    "QR_code": "qr code"
-                }
-            ],
-            "delivery_fee": "900",
-            "delivery_address": "4rd Cross #A148"
-        }
-        event_obj = create_event_model.create_event_model_from_dict(json_data)
-        event_controller.create_event(event_obj)
         response, status_code = admin_controller.admin_dashboard(request.session.get('uid'))
         return render(request, 'index.html', response)
     else:

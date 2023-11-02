5e2fe31ab90300bab272567338317d40335f87f2
santhosh kumar <santhosh.kumar@conigitalgroup.com>
2023-11-02 13:52:08 +0530

    settings password changed

--- Shagun_backend/settings.py
+++ Shagun_backend/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shagun',
         'USER': 'root',
-        'PASSWORD': 'Shagun@#123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
